9e34f1859e2d68392c5fd96f8ef0fa778b783dde
Emma LEJAY <emma.lejay@inrae.fr>
2022-02-02 12:21:30 +0100

    connect

--- api/tasks-get/index.js
+++ api/tasks-get/index.js
@@ -1,5 +1,9 @@
 module.exports = async function (context, req) {
     const mongoClient = require("mongodb").MongoClient;
+    const header = req.headers['x-ms-client-principal'];
+    const encoded = Buffer.from(header, 'base64');
+    const decoded = encoded.toString('ascii');
+    const user = JSON.parse(decoded);
     const client = await mongoClient.connect(process.env.COSMOSDB_CONNECTION_STRING);
     const database = client.db("tp_task");
     const response = await database.collection("tasks").find({
